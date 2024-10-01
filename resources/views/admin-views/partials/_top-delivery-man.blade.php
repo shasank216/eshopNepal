88e64fbfb9077e096771e1a528ff79fa3ea8b64e
Shasank Shrestha <shasankshrestha4@gmail.com>
2024-10-01 11:22:07 +0545

    admin dashboard icon changes

--- resources/views/admin-views/partials/_top-delivery-man.blade.php
+++ resources/views/admin-views/partials/_top-delivery-man.blade.php
@@ -1,6 +1,6 @@
 <div class="card-header">
     <h4 class="d-flex align-items-center text-capitalize gap-10 mb-0">
-        <img src="{{dynamicAsset(path: 'public/assets/back-end/img/top-customers.png')}}" alt="">
+        <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/top-customers.png')}}" alt="">
         {{translate('top_Delivery_Man')}}
     </h4>
 </div>
